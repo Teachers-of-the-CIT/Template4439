d2fb88a8a4f3a0d5a2c90ca584ce169397d58964
Avel <avel.khayrullov@.mail.ru>
2022-10-04 20:27:28 +0300

    Khayrullov add new window

---
+++ Template4439/4439_Khayrullov.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template4439
+{
+    /// <summary>
+    /// Логика взаимодействия для _4439_Khayrullov.xaml
+    /// </summary>
+    public partial class _4439_Khayrullov : Window
+    {
+        public _4439_Khayrullov()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
